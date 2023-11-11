5b41162220028280c14167adbaca863f2b85ccc0
alfaniq <148266686+alfaniq@users.noreply.github.com>
2023-11-11 11:17:13 -0700

    Update timer.cpp

--- Lab 5/src/timer.cpp
+++ Lab 5/src/timer.cpp
@@ -1,116 +1,54 @@
-// Author: 
-// Net ID: 
-// Date: 
-// Assignment: Lab 3
-//----------------------------------------------------------------------//
+/*
+## timer.cpp
+1.Implement a precise millisecond timer using timer 1 for switch
+debouncing
+*/
 
 #include "timer.h"
-
-//Global variable for counting ms and us
-volatile unsigned int delayCounter = 0;
-volatile unsigned int delayCounterUs = 0;
+#include <avr/io.h>
 
 /* Initialize timer 1, you should not turn the timer on here. Use CTC mode  .*/
-void initTimer1(){
-    TCCR1A &= ~( (1 << WGM10) | ( 1<< WGM11));
-    TCCR1B |= ( 1 << WGM12);
-    TCCR1B &= ~ ( 1 << WGM13);
-
-    // Enable Timer0 Compare Match A Interrupt
-    TIMSK1 |= (1 << OCIE1A);
+void initTimer1(){ 
+    TCCR1A &= ~ (1 << WGM10);
+    TCCR1A &= ~ (1 << WGM11);
+    TCCR1B |=   (1 << WGM12);
+    TCCR1B &= ~ (1 << WGM13);
 }
 
 /* This delays the program an amount of microseconds specified by unsigned int delay.
 */
 void delayMs(unsigned int delay){
-    delayCounter = delay;
-
-    // Set Prescalar to 64
-    TCCR1B |= (1 << CS11) | (1 << CS10);
-    TCCR1B &= ~(1 << CS12);
-
-    OCR1A = 249;
-
-    // Start timer at 0
-    TCNT1 = 0;
-
-    while(delayCounter > 0);
-    /*
-    TIFR1 |= (1 << OCF1A);
-
-    // Wait for the interrupt to handle the delay
-    while(delayCounterUs > 0){
-        TIFR1 |= (1 << OCF1A);
-        TCNT1 = 0;
-
-        while(( TIFR1 & (1 << OCF1A)) == 0);
-        delayCounterUs--;
-    }
-    */
-
-    TCCR1B &= ~( (1 <<  CS10) | (1 <<  CS11) | (1 <<  CS12));
-}
-
-/* Initialize timer 0, you should not turn the timer on here.
-* You will need to use CTC mode */
-void initTimer0(){
-    // Set timer to CTC mode
-    TCCR0A &= ~(1 << WGM00);
-    TCCR0A |=  (1 << WGM01);
-    TCCR0B &= ~(1 << WGM02);
-
-    // Enable Timer0 Compare Match A Interrupt
-    TIMSK0 |= (1 << OCIE0A);
-}
-
-/* This delays the program an amount specified by unsigned int delay.
-* Use timer 0. Keep in mind that you need to choose your prescalar wisely
-* such that your timer is precise to 1 millisecond and can be used for
-* 100-2000 milliseconds
-*/
-void delayUs(unsigned int delay){
-    delayCounter = delay;
+    unsigned int count = 0;
+    // set outout compare value
+    OCR0A = 249;
 
     // turn on clock with the CS bits and start counting
-    // Use Prescaleer of 8 (2 counts is  1 us)
-    TCCR0B |= (1 << CS01);
-    TCCR0B &= ~((1 << CS02)| (1 << CS00));
+    // Use Prescaleer of 8
+    TCCR0B |=  (1 << CS00);
+    TCCR0B |=  (1 << CS01);
+    TCCR0B &= ~(1 << CS02);
 
-    // Set compare value to 249 for 1 ms
-    OCR0A = 1;
 
-    // Start timer at 0
+    // set TCNT0 = 0
     TCNT0 = 0;
 
-    while(delayCounter > 0);
-
-    /*
+    // set output compare flag  TIFR0 down by writing a logic 1
     TIFR0 |= (1 << OCF0A);
 
-    // Wait for the interrupt to handle the delay
-    while(delayCounter > 0){
+    
+
+    // poll the flag OCF1A bit to see when it is raised
+     while (count < delay){
         TIFR0 |= (1 << OCF0A);
         TCNT0 = 0;
-
-        while(( TIFR0 & (1 << OCF0A)) == 0);
-        delayCounterUs--;
+        // while the flag bit OCF1A is down , do nothing
+        while (( TIFR0 & ( 1 << OCF0A)) == 0) ;
+        count++;
     }
-    */
-
-    // Turn off clock
-    TCCR0B &= ~( (1 << CS00) | (1 << CS01) | (1 << CS02) );
-}
-
-
-// This is the interrupt handler for Timer0 Compare Match A
-ISR(TIMER0_COMPA_vect){
-    if(delayCounter > 0){
-        delayCounter--;
-    }
-}
-
-ISR(TIMER1_COMPA_vect){
-    if(delayCounter > 0){
-        delayCounter--;
-    }
+    
+    // exit out we have our delay required
+    // turn off clock
+    TCCR0B &= ~(1 << CS02);
+    TCCR0B &= ~(1 << CS01); 
+    TCCR0B &= ~(1 << CS00);
 }
