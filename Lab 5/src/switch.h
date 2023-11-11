fadc98fe6a0f3c32cfcd5124a3383ea210f16de0
alfaniq <148266686+alfaniq@users.noreply.github.com>
2023-11-11 11:18:10 -0700

    Update switch.h

--- Lab 5/src/switch.h
+++ Lab 5/src/switch.h
@@ -4,8 +4,9 @@
 #ifndef SWITCH_H
 #define SWITCH_H
 
-void initSwitchPD0();
-void enableSwitch();
-void disableSwitch();
+void initSwitch();
+void initalarm();
+void turnon_alarm();
+void silence_alarm();
 
 #endif
