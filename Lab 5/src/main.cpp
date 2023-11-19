4bc01cafb50d7f65c672686fe39b8858051c5816
alfaniq <148266686+alfaniq@users.noreply.github.com>
2023-11-18 21:35:33 -0700

    Updated main.cpp

--- Lab 5/src/main.cpp
+++ Lab 5/src/main.cpp
@@ -1,7 +1,7 @@
 // Author:
 // Net ID:
 // Date:
-// Assignment:     Lab 3
+// Assignment: Lab 5
 //
 // Description: This file contains a programmatic overall description of the
 // program. It should never contain assignments to special function registers
@@ -11,13 +11,14 @@
 //----------------------------------------------------------------------//
 
 #include <Arduino.h>
+#include <avr/interrupt.h>
 #include <avr/io.h>
 
 #include "switch.h"
 #include "timer.h"
 #include "pwm.h"
 #include "i2c.h"
-
+#include "spi.h"
 
 
 #define MPU 0x68
@@ -34,15 +35,37 @@ volatile unsigned int x_value;
 volatile unsigned int y_value;
 volatile unsigned int z_value;
 
+//state machine for button
+typedef enum stateType_enum {
+  wait_press, debounce_press, wait_release, debounce_release
+} stateType;
+
+volatile stateType buttonState = wait_press; 
+
+//state machine for matrix
+typedef enum stateType_enum {
+  smiley_face, frowny_face
+} face;
+
+volatile face matrixFace= smiley_face; 
+
+
+
 
 int main(){
-
-  sei();
-
-  initTimer0();
-  initTimer1();
-
   Serial.begin(9600);
+
+  //initializations
+  initTimer1();
+  initPWMTimer3();
+  initPWMTimer4();
+  init_spi();
+  initSwitch();
+  initalarm();
+
+  //enable interrupts
+  sei();
+  
 
   initI2C();
   Serial.println("Completed init");
@@ -81,10 +104,55 @@ int main(){
     Serial.println(y_value);
     Serial.println(z_value);
     Serial.println("\n");
-    
 
-	}
+    switch (matrixFace) {
+      case smiley_face:
+        happy_face();
+      break;
+
+      case frowny_face:
+        frown_face();
+      break;
+
+      default:
+        matrixFace = smiley_face;
+      break;
+
+    }
+
+    switch(buttonState) { //waiting for switch to be pressed
+      case wait_press:
+      break;
+    
+      case debounce_press: 
+        buttonState = wait_release;
+        delayMs(1); 
+      break;
+      
+      case wait_release:
+      break;
+      
+      case debounce_release:
+        buttonState = wait_press;
+        delayMs(1);
+      break;
+    }
   
+    StopI2C_Trans();
+	}
   return 0;
 }
 
+// ISR for switch to stop the alarm noise
+ISR(INT0_vect) {
+  if(buttonState == wait_press)
+    {
+        buttonState = debounce_press;
+    }
+    else if (buttonState == wait_release) {
+     silence_alarm();
+      matrixFace = smiley_face;
+      buttonState = debounce_release;
+    }
+
+}
