f66d93a1d4913e5cf360c5e7fc65eeda6a767627
alfaniq <148266686+alfaniq@users.noreply.github.com>
2023-12-04 12:15:57 -0700

    Update rfid.cpp

--- Final Project/src/rfid.cpp
+++ Final Project/src/rfid.cpp
@@ -1,39 +1,57 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <string.h>
 
 #include "rfid.h"
 
 // Define RFID module register addresses
-#define MFRC522_REG_COMMAND     0x01
-#define MFRC522_REG_COM_IRQ     0x04
-#define MFRC522_REG_FIFO_DATA   0x09
+#define RC522_REG_COMMAND     0x01
+#define RC522_REG_COM_IRQ     0x04
+#define RC522_REG_FIFO_DATA   0x09
 
 // Define some commands (pg72 in data sheet)
-#define MFRC522_CMD_IDLE        0x00
-#define MFRC522_CMD_MEM         0x01
-#define MFRC522_CMD_RANDOM_ID   0x02
-#define MFRC522_CMD_CALC_CRC    0x03
-#define MFRC522_CMD_TRANSMIT    0x04
-#define MFRC522_CMD_NO_CMD      0x07
-#define MFRC522_CMD_SOFT_RESET  0x0F
+#define RC522_CMD_IDLE        0x00
+#define RC522_CMD_MEM         0x01
+#define RC522_CMD_RANDOM_ID   0x02
+#define RC522_CMD_CALC_CRC    0x03
+#define RC522_CMD_TRANSMIT    0x04
+#define RC522_CMD_NO_CMD      0x07
+#define RC522_CMD_SOFT_RESET  0x0F
+
+#define SS_PIN PB0
+
+// Define RFID tag ID to grant access
+const unsigned char validTagID[] = {0xDA, 0xE9, 0x2A, 0xFE};
+
+// Global variable to indicate RFID tag presence
+volatile bool tagPresentFlag = false;
+
+volatile unsigned char tagData[4];
+
+const unsigned char RST_PIN = 5;
 
 // Function to initialize SPI communication
 void SPI_Init() {
-    // Set MOSI(11), SCK(13), and SS(10) as output MISO(12) 
-    DDRB |= (1 << DDB2) | (1 << DDB1) | (1 << DDB0);
+    // Set MOSI(51), SCK(52), and SS as output, MISO(50) as input
+    DDRB |= (1 << DDB2) | (1 << DDB1) | (1 << DDB3);
+
+    // Set MISO(50) as input
+    //DDRB &= ~(1 << DDB3);
+
+    PORTE &= ~(1 << RST_PIN);
 
     // Enable SPI, Master mode, set clock rate fck/16
     SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0);
 }
 
 // Function to transmit and receive data via SPI
-unsigned char SPI_Transceive(unsigned char data) {
+char SPI_Transceive(char data) {
     // Start transmission
     SPDR = data;
 
     // Wait for transmission complete
-    while (!(SPSR & (1 << SPIF)))
-        ;
+    while (!(SPSR & (1 << SPIF)));
 
     // Return received data
     return SPDR;
@@ -42,26 +60,57 @@ unsigned char SPI_Transceive(unsigned char data) {
 // Function to initialize RFID module
 void initModule() {
     // Send a soft reset command to the MFRC522
-    PORTB &= ~(1 << PB0); // Set SS (Slave Select) low
-    SPI_Transceive(MFRC522_REG_COMMAND);
-    SPI_Transceive(MFRC522_CMD_SOFT_RESET);
-    PORTB |= (1 << PB0); // Set SS high
+    PORTB &= ~(1 << SS_PIN); // Set SS (Slave Select) low
+    SPI_Transceive(RC522_REG_COMMAND);
+    SPI_Transceive(RC522_CMD_SOFT_RESET);
+    PORTB |= (1 << SS_PIN); // Set SS high
 
     _delay_ms(50); // Wait for the soft reset to complete
+
+    // Enable external interrupt on the RFID module's IRQ pin
+    EIMSK |= (1 << INT0);   // Enable INT0
+    EICRA |= (1 << ISC01);  // Trigger on falling edge
+
 }
 
 // Function to read RFID data
-void readModule() {
-    // Send a command to the MFRC522 to start reading
-    PORTB &= ~(1 << PB0); // Set SS (Slave Select) low
-    SPI_Transceive(MFRC522_REG_COMMAND);
-    SPI_Transceive(MFRC522_CMD_TRANSMIT);
-    PORTB |= (1 << PB0); // Set SS high
+void readRFIDTag() {
+    // Send a command to the RC522 to start reading
+    PORTB &= ~(1 << SS_PIN); // Set SS (Slave Select) low
+    SPI_Transceive(RC522_REG_COMMAND);
+    SPI_Transceive(RC522_CMD_TRANSMIT);
+    PORTB |= (1 << SS_PIN); // Set SS high
 
-    // Wait for the MFRC522 to complete reading
+    // Wait for the RC522 to complete reading
+    while (!tagPresentFlag) {
+        // Wait for tag presence
+    }
+
+    tagPresentFlag = false;
 
     // Read the received data from the FIFO buffer
-    PORTB &= ~(1 << PB0); // Set SS (Slave Select) low
-    SPI_Transceive(MFRC522_REG_FIFO_DATA | 0x80); // Read command for FIFO
-    PORTB |= (1 << PB0); // Set SS high
+    PORTB &= ~(1 << SS_PIN); // Set SS (Slave Select) low
+    SPI_Transceive(RC522_REG_FIFO_DATA | 0x80); // Read command for FIFO
+
+    // Read tag data
+    for (unsigned char i = 0; i < 4; ++i) {
+        tagData[i] = SPI_Transceive(0x00);
+    }
+
+    PORTB |= (1 << SS_PIN); // Set SS high
+}
+
+// External interrupt service routine for RC522 module's IRQ pin
+ISR(INT0_vect) {
+    tagPresentFlag = true; // Set the tag presence flag
+}
+
+// Function to compare RFID tag ID
+unsigned char compareTagID(const volatile unsigned char* tagData, const unsigned char* constValidTagID){
+    for (unsigned char i = 0; i < 4; ++i) {
+        if (tagData[i] != validTagID[i]) {
+            return 0; // Not equal
+        }
+    }
+    return 1; // Equal
 }
